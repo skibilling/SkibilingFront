827556b00b772bc2ef182b09779807a701dbeda8
Kittyk-son <mazariegosaguilarjuliod@gmail.com>
2025-04-23 02:54:22 -0600

    Fixing problems with the last version and deleting some atributes that don't exist anymore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { InvoicePageComponent } from './components/pages/invoice-page/invoice-page.component';
 import { TicketsPageComponent } from './components/pages/tickets-page/tickets-page.component';
 import { DropBoxComponent } from "./components/atoms/drop-box/drop-box.component";
+import { PruebasComponent } from './components/pages/pruebas/pruebas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvoicePageComponent, TicketsPageComponent, DropBoxComponent],
+  imports: [RouterOutlet, InvoicePageComponent, TicketsPageComponent, DropBoxComponent, PruebasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
